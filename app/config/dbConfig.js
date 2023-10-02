9ea4c76ef7b39c1268ed04abfc15cd4672fd0d6c
Navin <sharma.navi@northeastern.edu>
2023-10-02 17:04:39 -0400

    initial commit

---
+++ app/config/dbConfig.js
@@ -0,0 +1,14 @@
+import env from 'dotenv';
+
+env.config();
+
+export default {
+    database: {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        dialect: process.env.dialect,
+        port: process.env.port,
+    },
+};
