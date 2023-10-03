716e764bcb781df157df86592ebc810da1369acb
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:39:01 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,16 +3,13 @@ import app from '../server.js';
 import { expect } from 'chai';
 
 describe("GET /v1/healthz", () => {
-  it("It should respond 200", (done) => {
+  it("It should respond 200", (done) => { // Add done callback here
     request(app)
       .get("/v1/healthz")
-      .end((err, response) => {
-        if (err) {
-          done(err); // Pass the error to Mocha
-        } else {
-          expect(response.statusCode).equal(200);
-          done(); // Signal to Mocha that the test is complete
-        }
+      .end((err, response) => { // Use end method to handle the response
+        if (err) return done(err); // If there's an error, pass it to done
+        expect(response.statusCode).equal(200);
+        done(); // Call done to signal that the test is complete
       });
   });
 });
