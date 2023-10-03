b918a60ee639ca7ebc2cf1f02eeb9ffeb5c7a9a6
Navin <sharma.navi@northeastern.edu>
2023-10-03 15:01:50 -0400

    testing script

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -4,11 +4,11 @@ env.config();
 
 export default {
     database: {
-        host: `${process.env.host}`,
-        user: `${process.env.user}`,
-        password: `${process.env.password}`,
-        database: `${process.env.database}`,
-        dialect: `${process.env.dialect}`,
-        port: `${process.env.port}`,
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database,
+        dialect: process.env.dialect,
+        port: process.env.port,
     },
 };
