a376128ddb5e0ffca1cfbc5c2f0e71d8a9ce5412
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:34:53 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,17 +3,15 @@ import app from '../server.js';
 import { expect } from 'chai';
 
 describe("GET /v1/healthz", () => {
-  it("should respond 200", (done) => {
+  it("It should respond 200", (done) => {
     request(app)
       .get("/v1/healthz")
       .end((err, response) => {
         if (err) {
-          done(err); // Call done with an error if there is one
+          done(err); // Pass the error to Mocha
         } else {
-          console.log("Response:", response.body);
-          console.log("Status Code:", response.statusCode);
           expect(response.statusCode).equal(200);
-          done(); // Call done without an error to indicate success
+          done(); // Signal to Mocha that the test is complete
         }
       });
   });
