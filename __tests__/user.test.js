9718ebf3439a80200c4d6c7136dbccebdfc379d5
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:07:48 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,9 +2,11 @@ import request from 'supertest';
 import app from '../server.js';
 import { expect } from 'chai';
 
-describe("GET /v1/healthz ", () => {
+describe("GET /v1/healthz", () => {
   it("It should respond 200", async () => {
     const response = await request(app).get("/v1/healthz");
+    console.log("Response:", response.body); // Log the response body
+    console.log("Status Code:", response.statusCode); // Log the status code
     expect(response.statusCode).equal(200);
   });
 });
