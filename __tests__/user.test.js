f5f97b13a981976f42773fe39a02e2b0747e4ec1
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:29:07 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,8 +3,18 @@ import app from '../server.js';
 import { expect } from 'chai';
 
 describe("GET /v1/healthz", () => {
-  it("It should respond 200", async () => {
-    const response = await request(app).get("/v1/healthz");
-    expect(response.statusCode).equal(200);
+  it("should respond 200", (done) => {
+    request(app)
+      .get("/v1/healthz")
+      .end((err, response) => {
+        if (err) {
+          done(err); // Call done with an error if there is one
+        } else {
+          console.log("Response:", response.body);
+          console.log("Status Code:", response.statusCode);
+          expect(response.statusCode).equal(200);
+          done(); // Call done without an error to indicate success
+        }
+      });
   });
 });
