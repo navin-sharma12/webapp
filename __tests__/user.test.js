b9d046ca07c3147a68fee0b1311f39c87596b613
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:22:02 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -4,8 +4,7 @@ import { expect } from 'chai';
 
 describe("GET /v1/healthz", () => {
   it("It should respond 200", async () => {
-    this.timeout(10000);
     const response = await request(app).get("/v1/healthz");
     expect(response.statusCode).equal(200);
-  });
+  }).timedOut(10000);
 });
