11be6d9afb38c35783173539b2ad92bcc1056a19
Navin <sharma.navi@northeastern.edu>
2023-10-03 19:41:42 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,13 +3,8 @@ import app from '../server.js';
 import { expect } from 'chai';
 
 describe("GET /v1/healthz", () => {
-  it("It should respond 200", (done) => { // Add done callback here
-    request(app)
-      .get("/v1/healthz")
-      .end((err, response) => { // Use end method to handle the response
-        if (err) return done(err); // If there's an error, pass it to done
-        expect(response.statusCode).equal(200);
-        done(); // Call done to signal that the test is complete
-      });
-  });
+  it("It should respond 200", async () => { 
+    const response = await request(app).get("/v1/healthz");
+    expect(response.statusCode).equal(200);
+  }).timeout(60000);
 });
