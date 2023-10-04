eefcde59108be06b842c130e0b95a005bd84ee3d
Navin <sharma.navi@northeastern.edu>
2023-10-04 19:58:12 -0400

    final changes

--- app/services/userService.js
+++ app/services/userService.js
@@ -12,7 +12,7 @@ const initializeDatabase=async () => {
         await db.sequelize.sync({ create: true }); // Use alter: true to update existing schema
 
         // Load and insert data from CSV files
-        const csvData = fs.readFileSync(path.join('/Users/navinsharma/Desktop/Uni Courses/Sem 3/Cloud/webapp/users.csv'), 'utf-8');
+        const csvData = fs.readFileSync(path.join('/root/opt/webapp/users.csv'), 'utf-8');
         const rows = csvData.split('\n').map((row) => row.split(','));
 
         for (let i = 1; i < rows.length; i++) {
