0c0c97e59f9d12d3d05892252f7a947ca14bde2e
Navin <sharma.navi@northeastern.edu>
2023-10-03 22:28:49 -0400

    testing script

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,8 +1,15 @@
 import request from 'supertest';
 import app from '../server.js';
 import { expect } from 'chai';
+import config from '../app/config/dbConfig.js';
 
 describe("GET /v1/healthz", () => {
+  console.log(config.database.dialect);
+  console.log(config.database.user);
+  console.log(config.database.pd);
+  console.log(config.database.user);
+  console.log(config.database.port);
+  console.log(config.database.database);
   it("It should respond 200", (done) => { // Use the 'done' callback
     request(app)
       .get("/v1/healthz")
