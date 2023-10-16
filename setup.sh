c8c59a07dbb6bc15abaf4029db70bb080ea084d5
Navin <114364597+navin-sharma12@users.noreply.github.com>
2023-10-15 20:34:01 -0400

    Assignment05 (#72)
    * updated packer file
    
    * workflow updated
    
    * workflow updated
    
    * workflow updated
    
    * Delete .env
    
    * Delete package-lock.json
    
    * updated workflow
    
    * updated workflow
    
    * workflow seperated for format and validate
    
    * workflow seperated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ sleep 30
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('Jay@1998');FLUSH PRIVILEGES;"
-printf 'Jay@1998\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON healthCheck.* TO 'root'@'localhost' IDENTIFIED BY 'Jay@1998';"
-mysql -u root -pJay@1998 -Bse "CREATE DATABASE cloud;"
-mysql -u root -pJay@1998 -Bse "SHOW DATABASES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('15672N@vin');FLUSH PRIVILEGES;"
+printf '15672N@vin\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON healthCheck.* TO 'root'@'localhost' IDENTIFIED BY '15672N@vin';"
+mysql -u root -p15672N@vin -Bse "CREATE DATABASE cloud;"
+mysql -u root -p15672N@vin -Bse "SHOW DATABASES;"
 cd /root/opt/
 unzip -o webapp.zip
 cd webapp
