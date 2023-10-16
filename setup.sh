b4d4f8978fbdddf4194c95b836c8941257b7d005
Navin <114364597+navin-sharma12@users.noreply.github.com>
2023-10-15 23:25:10 -0400

    Assignment05 (#74)
    * updated packer file
    
    * workflow updated
    
    * workflow updated
    
    * workflow updated
    
    * Delete .env
    
    * Delete package-lock.json
    
    * updated workflow
    
    * updated workflow
    
    * workflow seperated for format and validate
    
    * workflow seperated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-sleep 30
-
-mkdir opt
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('15672N@vin');FLUSH PRIVILEGES;"
 printf '15672N@vin\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON healthCheck.* TO 'root'@'localhost' IDENTIFIED BY '15672N@vin';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON cloud.* TO 'root'@'localhost' IDENTIFIED BY '15672N@vin';"
 mysql -u root -p15672N@vin -Bse "CREATE DATABASE cloud;"
 mysql -u root -p15672N@vin -Bse "SHOW DATABASES;"
-cd /root/opt/
-unzip -o webapp.zip
+sudo mkdir opt
+sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
+sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
+cd opt
+sudo unzip -o webapp.zip
 cd webapp
-npm i
-npm start
+sudo npm i
+sudo npm run test
+# sudo npm start
