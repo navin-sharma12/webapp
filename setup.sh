9d5493ec66c204ea4194493322f329f84d999cca
Navin <sharma.navi@northeastern.edu>
2023-10-16 22:32:55 -0400

    testing

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-sudo apt-get update
+echo "PASSWORD=${PD}"
+echo "DATABASE=${DATABASE}"
+echo "USER=${USER}"
+
+sudo apt-get u{PD}ate
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('15672N@vin');FLUSH PRIVILEGES;"
-printf '15672N@vin\n n\n n\n n\n n\n n\n y\n' | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON cloud.* TO 'root'@'localhost' IDENTIFIED BY '15672N@vin';"
-mysql -u root -p15672N@vin -Bse "CREATE DATABASE cloud;"
-mysql -u root -p15672N@vin -Bse "SHOW DATABASES;"
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${PD}');FLUSH PRIVILEGES;"
+printf `${PD}\n n\n n\n n\n n\n n\n y\n` | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO 'root'@'localhost' IDENTIFIED BY '${PD}';"
+mysql -u root -p${PD} -Bse "CREATE DATABASE ${DATABASE};"
+mysql -u root -p${PD} -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
