dfdfbb58f0cfccbee7949ee4b3457efe298f90d8
Navin <114364597+navin-sharma12@users.noreply.github.com>
2023-10-16 22:46:40 -0400

    Assignment05 (#78)
    * updated packer file
    
    * workflow updated
    
    * workflow updated
    
    * workflow updated
    
    * Delete .env
    
    * Delete package-lock.json
    
    * updated workflow
    
    * updated workflow
    
    * workflow seperated for format and validate
    
    * workflow seperated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * workflow updated for format and validate
    
    * testing
    
    * testing
    
    * testing bash file

--- setup.sh
+++ setup.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-echo "PASSWORD=${PD}"
-echo "DATABASE=${DATABASE}"
-echo "USER=${USER}"
-
 sudo apt-get u{PD}ate
 sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${PD}');FLUSH PRIVILEGES;"
-printf `${PD}\n n\n n\n n\n n\n n\n y\n` | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO 'root'@'localhost' IDENTIFIED BY '${PD}';"
-mysql -u root -p${PD} -Bse "CREATE DATABASE ${DATABASE};"
+printf "${PD}\n n\n n\n n\n n\n n\n y\n" | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '${PD}';"
+mysql -u root -p${PD} -Bse "CREATE DATABASE $DATABASE;"
 mysql -u root -p${PD} -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
