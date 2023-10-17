975bc8eea781e838830fd4a0b8a1147ac3feab51
Navin <sharma.navi@northeastern.edu>
2023-10-16 22:45:23 -0400

    testing bash file

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${PD}');FLUSH PRIVILEGES;"
-printf `${PD}\n n\n n\n n\n n\n n\n y\n` | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON ${DATABASE}.* TO 'root'@'localhost' IDENTIFIED BY '${PD}';"
-mysql -u root -p${PD} -Bse "CREATE DATABASE ${DATABASE};"
+printf "${PD}\n n\n n\n n\n n\n n\n y\n" | sudo mysql_secure_installation
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '${PD}';"
+mysql -u root -p${PD} -Bse "CREATE DATABASE $DATABASE;"
 mysql -u root -p${PD} -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
