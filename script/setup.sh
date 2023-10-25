0b65bad97af3cc0eebb496e70522a75d434de32e
Navin <sharma.navi@northeastern.edu>
2023-10-25 19:50:56 -0400

    final commit after location

--- script/setup.sh
+++ script/setup.sh
@@ -5,13 +5,12 @@ sudo apt-get upgrade -y
 sudo apt-get install systemd
 sudo apt install unzip
 sudo apt install nodejs npm -y
-sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
-cd opt
-sudo unzip -o webapp.zip -d /opt/csye6225/webapp
-cd webapp
-sudo npm i
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
+sudo unzip -o /opt/csye6225/webapp.zip -d /opt/csye6225/
+cd /opt/csye6225/webapp
+sudo npm i
 sudo cp /home/admin/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
