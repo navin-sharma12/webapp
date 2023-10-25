a7c0750ff59b0d581051d007e41173d8e5f32c81
Navin <sharma.navi@northeastern.edu>
2023-10-24 20:04:04 -0400

    updated pulumi with rds instance and userdata

--- script/setup.sh
+++ script/setup.sh
@@ -5,22 +5,13 @@ sudo apt-get upgrade -y
 sudo apt-get install systemd
 sudo apt install unzip
 sudo apt install nodejs npm -y
-sudo apt install mariadb-server -y
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$PD');FLUSH PRIVILEGES;"
-printf "$PD\n n\n n\n n\n n\n n\n y\n" | sudo mysql_secure_installation
-sudo mysql -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO 'root'@'localhost' IDENTIFIED BY '$PD';"
-mysql -u root -p$PD -Bse "CREATE DATABASE $DATABASE;"
-mysql -u root -p$PD -Bse "SHOW DATABASES;"
 sudo mkdir opt
 sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
-# sudo mv /home/admin/users.csv /home/admin/opt/webapp/users.csv
 cd opt
 sudo unzip -o webapp.zip
 cd webapp
 sudo npm i
-# sudo npm run test
 sudo cp /home/admin/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
 sudo systemctl start webapp.service
-# sudo npm start
