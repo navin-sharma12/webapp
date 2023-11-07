a0659b46c7719d91624b99c5e789764332869987
Navin <sharma.navi@northeastern.edu>
2023-11-07 13:52:39 -0500

    script for installation updated

--- script/setup.sh
+++ script/setup.sh
@@ -9,6 +9,7 @@ sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/am
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo cp /home/admin/webapp.service /etc/systemd/system/
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 sudo unzip -o /opt/csye6225/webapp.zip -d /opt/csye6225/webapp
 sudo cp /home/admin/cloudwatch-config.json /opt/csye6225/webapp/cloudwatch-config.json
@@ -20,7 +21,6 @@ sudo cp /home/admin/cloudwatch-config.json /opt/csye6225/webapp/cloudwatch-confi
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 sudo cp /opt/csye6225/webapp/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 sudo npm i
-sudo cp /home/admin/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
 sudo systemctl start webapp.service
